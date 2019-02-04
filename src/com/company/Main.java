3d4a2d01a41e1d2ccd9bd00149cce0be5306a439
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-02-04 11:36:47 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Cost \t  Quantity \t Total");
+    System.out.println("$1000.00\t4 \t    $4,000.00");
+    System.out.println("$  50.00\t8  \t      $400.00");
+    System.out.println("______________________________");
+    System.out.println("TOTAL: \t    12\t    $4,400.00");
+    }
+}
